02832e2e5831a20714d3cbe3592557e5c0912c02
Yoan <mrts.yoan@gmail.com>
2024-11-28 14:30:56 +0100

    test

--- src/CbVerifier.php
+++ src/CbVerifier.php
@@ -6,8 +6,10 @@ class CbVerifier
 {
     function Luhn($numero, $longueur)
     {
-        if (strlen($numero) == $longueur)
-        {
+        if (strlen($numero) == $longueur && preg_match(
+            "#[0-9]{" . $longueur . "}#i",
+            $numero
+        )) {
             return true;
         } else {
             return false;
