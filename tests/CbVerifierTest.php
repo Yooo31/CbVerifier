6ad6c5bcd55d5ed5ee7600f75d6cd7ea42ad4f3a
Yoan <mrts.yoan@gmail.com>
2024-11-28 14:43:42 +0100

    test

--- tests/CbVerifierTest.php
+++ tests/CbVerifierTest.php
@@ -27,4 +27,10 @@ class CbVerifierTest extends TestCase
     $this->assertTrue($this->verifier->Luhn('1234567890123456', 16));
     $this->assertFalse($this->verifier->Luhn('1234567890aaaaaa', 16));
   }
+
+  public function testValidCard(): void
+  {
+    $this->assertTrue($this->verifier->Luhn('4603433767975432', 16));
+    $this->assertFalse($this->verifier->Luhn('1234567890123456', 16));
+  }
 }
